3e39142e3142779a5fe770b479c02c697a555a81
rajputnitiket <nitiketrajput1@gmail.com>
2023-11-06 15:42:45 +0530

    06-11

--- todoConLocal/src/App.jsx
+++ todoConLocal/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import {TodoProvider} from "./context"
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -14,6 +14,30 @@ function App() {
   const updateTodo =(id, todo) =>{
     setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo )))
   }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !==id))
+  }
+
+  const ToggleComplete = (id) =>
+  {
+    setTodos((prevTodo) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed :!prevTodo.completed} : prevTodo))
+  }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if (todos && todos.length >0){
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+
+  }, [todos])
+  
+  
 
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, ToggleComplete}}>
