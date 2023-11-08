9881db5c2aff9e5f593ac4b7294d52f7a5b0cfd6
rajputnitiket <nitiketrajput1@gmail.com>
2023-11-08 17:20:31 +0530

    08-11

--- todoConLocal/src/App.jsx
+++ todoConLocal/src/App.jsx
@@ -21,9 +21,13 @@ function App() {
     setTodos((prev) => prev.filter((todo) => todo.id !==id))
   }
 
-  const ToggleComplete = (id) =>
-  {
-    setTodos((prevTodo) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed :!prevTodo.completed} : prevTodo))
+
+  const toggleComplete = (id) => {
+    //console.log(id);
+    setTodos((prev) => 
+    prev.map((prevTodo) => 
+      prevTodo.id === id ? { ...prevTodo, 
+        completed: !prevTodo.completed } : prevTodo))
   }
 
   useEffect(() => {
@@ -42,20 +46,20 @@ function App() {
   
 
   return (
-    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, ToggleComplete}}>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
      <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
-                        {TodoForm}
+                        <TodoForm/>
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
                         {
                           todos.map((todo) =>
                           <div key={todo.id} className='w-full'>
-                            <TodoItem todo={todo}/>
+                            <TodoItem todo={todo} />
                           </div>
                           
                           )
