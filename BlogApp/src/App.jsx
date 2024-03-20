4e7803497c7bfd8134645e050acab2869bdfac9c
rajputnitiket <nitiketrajput1@gmail.com>
2024-03-20 17:37:14 +0530

    niti

--- BlogApp/src/App.jsx
+++ BlogApp/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  console.log(import.meta.env.VITE_APPWRITE_URL)
 
   return (
     <>
-        <h1>A blog app</h1>
+      <h1> A Blog App</h1>
     </>
   )
 }
